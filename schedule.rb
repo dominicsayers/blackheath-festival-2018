bc341e30e522cd6f7b32b3195796cb10972c8b0b
Dominic Sayers <dominic@sayers.cc>
2018-01-29 18:22:04 +0000

    fixup! v5

--- schedule.rb
+++ schedule.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 require_relative '../tournament_schedule/lib/tournament'
-threads = (7..11).map { |age_group| Thread.new { Tournament.new(filename: "U#{age_group}.yml").schedule } }
-threads.each(&:join)
+# threads = (7..11).map { |age_group| Thread.new { Tournament.new(filename: "U#{age_group}.yml").schedule } }
+# threads.each(&:join)
+Tournament.new(filename: "U11.yml").schedule
