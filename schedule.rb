a8e4edd1a41c6749fbfb06e7581cca0dba7abaef
Dominic Sayers <dominic@sayers.cc>
2018-01-29 13:30:33 +0000

    Schedule all tournaments

---
+++ schedule.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+require_relative '../tournament_schedule/lib/tournament'
+threads = (7..11).map { |age_group| Thread.new { Tournament.new(filename: "U#{age_group}.yml").schedule } }
+threads.each(&:join)
