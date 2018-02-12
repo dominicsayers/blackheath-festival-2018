418225d82bfb8a6dc3b2ad765b2be22d11d3e1e0
Dominic Sayers <dominic@sayers.cc>
2018-02-12 16:57:51 +0000

    v8

--- schedule.rb
+++ schedule.rb
@@ -3,4 +3,4 @@
 require_relative '../tournament_schedule/lib/tournament'
 # threads = (7..11).map { |age_group| Thread.new { Tournament.new(filename: "U#{age_group}.yml").schedule } }
 # threads.each(&:join)
-Tournament.new(filename: "U11.yml").schedule
+Tournament.new(filename: "U10.yml").schedule
